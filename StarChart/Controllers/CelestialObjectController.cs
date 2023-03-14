01c29514fe03f2932a0fb3943604067e5b0aefc7
Jeremy Farago <jeremy-917@hotmail.com>
2023-03-14 15:20:03 -0400

    Module 5 Check

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Internal;
 using StarChart.Data;
+using StarChart.Models;
 
 namespace StarChart.Controllers
 {
@@ -13,7 +16,46 @@ namespace StarChart.Controllers
         {
             _context = context;
         }
-
+        [HttpGet("{id:int}")]
         
+        public IActionResult GetById(int id)
+        {
+            var item = _context.CelestialObjects.FirstOrDefault(x => x.Id == id);
+            if (item == null) return NotFound();
+
+            if(item.Id == item.OrbitedObjectId)
+            {
+                item.Satellites.Add(item);
+
+            }
+
+            return Ok(item.Id);
+
+        }
+
+        [HttpGet("{name}")]
+        public IActionResult GetByName(string name)
+        {
+            var items = _context.CelestialObjects.Where(x => x.Name == name);
+            if (items == null) return NotFound();
+            foreach (var item in items)
+            {
+                item.Satellites.Add(item);
+            }
+            return Ok(items);
+
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var items = _context.CelestialObjects;
+            foreach (var item in items)
+            {
+                item.Satellites.Add(item);
+            }
+            return Ok(items);
+        }
+
+
     }
 }
